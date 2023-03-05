e8c9ae5775fec2af9667ca06756282f4857b8620
dec0de284 <76246537+dec0de284@users.noreply.github.com>
2023-03-05 18:33:22 +0800

    Add dict-mach1 project

---
+++ dict-mach1/main.py
@@ -0,0 +1,48 @@
+# 1.4 Getting Started With Python
+# print("Hello, World!")
+# print("I love cakes!")
+# print('I love cakes!')
+
+# 1.7 Variables
+# x = 7
+# y = "cake"
+# print(x)
+# print(y)
+# favorite_food = "pizza"
+# friend_name = "Tina"
+# favorite_drink = "soda"
+# print("I like " + favorite_food + ".")
+# print("I like eating " + favorite_food + " with my friend, " + friend_name + ".")
+# print("We like to drink " + favorite_drink + ", too.")
+
+# 1.10 Variables
+# character_job = "pirate"
+# character_ride = "ship"
+# character_souvenir = "gold"
+# character_pet = "parrot"
+# print("There was once a " + character_job + " who loved adventure.")
+# print("The " + character_job + " would take her " + character_ride + " to unknown places.")
+# print("She brings home a lot of " + character_souvenir + ".")
+# print("Then she goes home to her pet " + character_pet + ", Chuckles.")
+
+# 1.14 Coding Exercise
+# print("Roses are red, violets are blue")
+# if 10 > 7:
+#     print("Ten is greater than seven!")
+# if 16 < 42:
+#     print("Sixteen is less than forty-two!")
+# print("A long time ago in a galaxy far, far away...")
+
+# 2.1 What are Functions?
+def hello_function():
+    print("Hello!")
+
+
+hello_function()
+
+
+def hello_function(name):
+    print("Hello! " + name + "!")
+
+
+hello_function("Rose")
